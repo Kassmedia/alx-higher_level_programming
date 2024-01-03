96abc81b967e6f6fa4cff91aeb22134d90886428
wahab <wabsid01@gmail.com>
2024-01-03 07:13:19 -0800

    last digit is love

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-big = "and is greater than 5"
-small = "and is less than 6 and not 0"
-draw = "and is 0"
-
-if number > 0:
-    end = number % 10
+if number < 0:
+    rem = number % -10
 else:
-    end = ((number * -1) % 10) * -1
-
-if end > 5:
-    final = big
-elif end == 0:  # Use `==` for comparison
-    final = draw
-elif end < 6 and end != 0:  # Correctly check for non-zero values
-    final = small
-
-print("last digit of", number, "is", end, final)
-
+    rem = number % 10
+print("Last digit of", number, "is", rem, end=' ')
+if rem > 5:
+    print("and is greater than 5")
+elif rem == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
