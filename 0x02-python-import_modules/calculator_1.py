282468e4a524707f12a5e8f7b88c4dc590f4703c
wahab <wabsid01@gmail.com>
2024-01-07 11:38:27 -0800

    1-calculation.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,11 +1,51 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculation_1 import add, sub, mul, div
-    a = 10
-    b = 5
-    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a,b)))
-    print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a,b)))
-    print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a,b)))
-    print("{0:d} / {1:d} = {2:d}".format(a, b, div(a,b)))
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
