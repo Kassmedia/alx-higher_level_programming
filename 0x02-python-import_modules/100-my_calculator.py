973fcc3f47779914db30e7b35b7548c46b501494
wahab <wabsid01@gmail.com>
2024-01-09 03:53:11 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
 
-    ng = len(sys.argv) - 1
-    if ng != 3:
-        print("Usage: . /100-my_calculator.py <a> <operator> <b>")
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    co = sys.argv[2]
-    if co != '+' and co != '-' and co != '*' and co != '/':
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
@@ -16,14 +16,11 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if co == '+':
+    if op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    elif co == '-':
+    elif op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif co == '*':
+    elif op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
     else:
         print("{} / {} = {}".format(a, b, div(a, b)))
-
-
-
