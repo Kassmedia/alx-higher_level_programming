84b6f71a9ebba97ae67cda7103b02e08d3daf450
wahab <wabsid01@gmail.com>
2024-01-10 09:57:21 -0800

    4-pray

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,5 @@
 def new_in_list(my_list, idx, element):
     copy_list = my_list.copy()
     if idx >= 0 and idx < len(my_list):
-        copy_list[idx] = element
-    return copy_list
-    
+        new_list[idx] = element
+    return (copy_list)
