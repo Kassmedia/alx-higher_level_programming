4f36dedea20aa34a59a131d2bbfdd9343cb45ac5
wahab <wabsid01@gmail.com>
2024-01-12 04:58:37 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (max(a_dictionary))
-    else:
-        return None
-
+        max_value = max(a_dictionary, key=a_dictionary.get)
+        return (max_value)
