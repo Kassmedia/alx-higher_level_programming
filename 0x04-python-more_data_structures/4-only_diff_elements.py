061f8fca40e6bc284cb53b1eb2e4aff8f1ba9c76
wahab <wabsid01@gmail.com>
2024-01-12 00:49:43 -0800

    3-main.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    check = set_1.union(set_2)
-    return check
+    check_1 = set_1.difference(set_2)
+    check_2 = set_2.difference(set_1)
+    return check_1.union(check_2)
