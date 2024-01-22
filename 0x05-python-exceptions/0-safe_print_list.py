99a6d922e4a4e2100b37fb7d383fa24fa03ba6ea
wahab <wabsid01@gmail.com>
2024-01-22 07:19:27 -0800

    0-main.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for a in my_list[:x]:
             print("{}".format(a), end="")
-            load = load + 1
+            load += 1
     except:
         pass
     print()
