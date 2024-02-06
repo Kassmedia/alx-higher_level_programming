fbe6df459064647d89735a66ab35eb9d4f0f1990
wahab <wabsid01@gmail.com>
2024-02-06 09:48:33 -0800

    rectangle inherit

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().init__(size, size)
+        super().__init__(size, size)
 
     def area(self):
         """Returns the area of the current Square instance. """
@@ -23,3 +23,4 @@ class Square(Rectangle):
     def __str__(self):
         """Return the representation of the square instance."""
         return "[Square] {}/{}".format(self.__size, self.__size)
+
