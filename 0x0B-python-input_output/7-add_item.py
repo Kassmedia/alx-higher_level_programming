88c77fcc1cd4856cef056914376b92d2d70803bf
root <wabsid01@gmail.com>
2024-02-07 12:22:58 +0000

    3-to_json_string.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" Adds all arguments to a Python list and saves them to a file """
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv[1:]
-
-try:
-    args_list_in_file = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    args_list_in_file = []
-
-args_list_in_file.extend(args)
-
-save_to_json_file(args_list_in_file, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
