d8351337e136c061f2b11183f83288fe6bff8d2a
root <wabsid01@gmail.com>
2024-02-07 11:55:46 +0000

    3-to_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" Defines a function that returns JSON representation of an object """
+""" Defines a function that returns a Python object from JSON string """
 import json
+
 
 def from_json_string(my_str):
     """
-        Returns the JSON representation of an object (string)
+        Returns an object (Python data structure) represented by a JSON string
 
         Args:
-            my_obj (obj): The object to return JSON representation
+            my_str (string): The JSON string to convert to an object
 
-        Return: (string) JSON representation of my_obj
+        Return: (object) Python object represented by my_str
     """
     return json.loads(my_str)
