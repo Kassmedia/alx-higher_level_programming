e1c8d2852c7e2275a4acda32efed768a6c361f56
wahab <wabsid01@gmail.com>
2024-04-08 17:30:41 -0700

    java

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//The first line: “C is fun” The second line: “Python is cool” The third line: “JavaScript is amazing”
+
+let a = 'C is fun';
+let b = 'Python is cool';
+let c = 'JavaScript is amazing';
+console.log(a);
+console.log(b);
+console.log(c);
