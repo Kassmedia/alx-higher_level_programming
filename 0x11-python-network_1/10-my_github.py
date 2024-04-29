90bc09baf31198385145bf1dd4e365e007ba5596
wahab <wabsid01@gmail.com>
2024-04-29 08:06:32 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == '__main__':
+    URL = 'https://api.github.com/users/{}'.format(argv[1])
+    req = requests.get(URL,
+                     authen=HTTPBasicAuth(argv[1], argv[2]))
+    print(req.json().get('id'))
