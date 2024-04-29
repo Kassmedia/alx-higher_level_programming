55325515ec8bce07f36eb57d688b19d757413330
wahab <wabsid01@gmail.com>
2024-04-29 07:13:07 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status"""
 
@@ -7,10 +6,9 @@ if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/
-                                status/') as resp:
-        ready = resp.read()
+                                status/') as response:
+        html = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(ready)))
-        print('\t- content: {}'.format(ready))
-        print('\t- utf8 content: {}'.format(ready.decode("utf-8", "replace")))
-
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
